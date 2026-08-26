4121c36a666e896baa457408192ff1cffe26bb65
Rucostaa <80977479+Rucostaa@users.noreply.github.com>
2026-08-26 14:29:16 +0100

    done

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ const PORT = process.env.PORT || 5000;
 |
 */
 
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://menupio.pt",
+];
 
 app.use(
   cors({
